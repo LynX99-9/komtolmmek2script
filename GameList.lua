a1cb5b50eb6a12893794b93e077efb5fe487e02c
LynX99-9 <kontolodon887297@gmail.com>
2026-05-06 22:01:48 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -13,7 +13,7 @@ local games = {
     [89469502395769]  = "https://raw.githubusercontent.com/LynX99-9/komtolmmek2/refs/heads/main/kick%20a%20lucky%20block",
     [131346454575416]  = "https://raw.githubusercontent.com/LynX99-9/komtolmmek2/refs/heads/main/mini%20war",
     [107095834793267] = "https://raw.githubusercontent.com/LynX99-9/komtolmmek2/refs/heads/main/Oil%20empire",
-    [104119360583174] = "",
+    [104119360583174] = "https://raw.githubusercontent.com/LynX99-9/komtolmmek2/refs/heads/main/Kick%20a%20LatamBlock",
 }
 
 return games
