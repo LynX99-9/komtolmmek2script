d85d94d5e322704e16a2cc5895acbc3e8a1b21a8
LynX99-9 <kontolodon887297@gmail.com>
2026-05-30 12:37:00 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -21,6 +21,7 @@ local games = {
     [95082159892680] = "https://raw.githubusercontent.com/LynX99-9/komtolmmek2/refs/heads/main/%2B1%20SKE",
     [129118369937980] = "https://raw.githubusercontent.com/LynX99-9/komtolmmek2/refs/heads/main/Eagle%20Nation",
     [136801880565837] = "https://raw.githubusercontent.com/LynX99-9/komtolmmek2/refs/heads/main/Flick",
+    [107646426076756] = "https://raw.githubusercontent.com/LynX99-9/komtolmmek2/refs/heads/main/Build%20a%20Ring%20farm",
 }
 
 return games
